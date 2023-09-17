cfd82ce89792ee163a3740846908fd205fe9992f
MEGHA_KUMARI <megha.10kumarii@gmail.com>
2023-09-17 18:47:01 +0530

    Created stack.cpp

---
+++ STACK/stack.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Stack {
+    public:
+        int *arr;
+        int top;
+        int size;
+
+    Stack(int size) {
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push( int element) {
+        if(size - top > 1) {
+            top++;
+            arr[top] = element;
+        }
+        else{
+            cout << "Stack OverFlow" << endl;
+        }
+    }
+
+    void pop() {
+        if(top >=0 ) {
+            top--;
+        }
+        else{
+            cout << "Stack UnderFlow" << endl;
+        }
+    }
+
+    int peek() {
+        if(top >=0 )
+            return arr[top];
+        else
+        {
+            cout << "Stack is Empty" << endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty() {
+        if( top == -1) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+
+int main() {
+    Stack st(2);
+
+    st.push(2);
+    st.push(5);
+    cout << st.peek() << endl;
+
+    
+    return 0;
+}
